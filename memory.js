b54c4249181bcef3a9b369d7ad6d5308ffd471c2
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-05-22 23:27:23 +0300

    js// Declaring the boxes and body variables

--- memory.js
+++ memory.js
@@ -4,3 +4,10 @@ let red_audio = document.getElementById("red_audio");
 let yellow_audio = document.getElementById("yellow_audio");
 let blue_audio = document.getElementById("blue_audio");
 let wrong_audio = document.getElementById("wrong_audio");
+
+// Declaring variables for selecting the boxes and the body
+let green_box = document.getElementsByClassName("top_boxes")[0];
+let red_box = document.getElementsByClassName("top_boxes")[1];
+let yellow_box = document.getElementsByClassName("bottom_boxes")[0];
+let blue_box = document.getElementsByClassName("bottom_boxes")[1];
+let body = document.getElementById("body");

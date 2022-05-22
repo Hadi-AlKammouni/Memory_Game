eca936b1c6921d8ca6c6a91f1c2018439f24704a
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-05-22 23:26:28 +0300

    js// Declaring the audio variables

---
+++ memory.js
@@ -0,0 +1,6 @@
+// Declaring audio variable for each color + wrong audio 
+let green_audio = document.getElementById("green_audio");
+let red_audio = document.getElementById("red_audio");
+let yellow_audio = document.getElementById("yellow_audio");
+let blue_audio = document.getElementById("blue_audio");
+let wrong_audio = document.getElementById("wrong_audio");
